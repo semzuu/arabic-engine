3eb901015d20c2be7f7010cd2715f1078c7368fb
bari <barikalil404@gmail.com>
2026-02-15 13:27:17 +0100

    Added the HashTable

---
+++ include/HashTable.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "Scheme.hpp"
+#include <vector>
+#include <list>
+
+class HashTable {
+	private:
+		std::vector<std::list<Scheme>> table;
+		size_t hash(const std::string& key);
+	
+	public:
+		HashTable(size_t size = 10);
+		void insert(const Scheme& s);
+		void remove(const std::string& name);
+		Scheme* find(const std::string& name);
+};
