216193ef1bb94d9c3e6e8cee2828b40493bb3956
Mohamed Khmissi <mohamedkhmissi69+git@gmail.com>
2026-02-21 13:00:26 +0100

    Changed tree implementation
    Changed tree insertion and search to iterative functions instead of
    using recursion

--- src/Tree.cpp
+++ src/Tree.cpp
@@ -5,37 +5,49 @@ Tree::Tree() {
 	root = nullptr;
 }
 // Insert a new node 
-Node *Tree::insert_impl(Node *node, const std::string& value) {
-	if (!node) return new Node{
-		{value, {}},
-		nullptr,
-		nullptr
-	};
-	if (value <= node->data.value) {
-		node->left = insert_impl(node->left, value);
-	}
-	else if (value > node->data.value) {
-		node->right = insert_impl(node->right, value);
-	}
-	return node;
-}
-
 void Tree::insert(const std::string& value) {
-	root = insert_impl(root, value);
-}
-// Searsh for a giving root in the tree and return its data struct pointer
-Node *Tree::search_impl(Node *node, const std::string& value) {
-	if (!node) return nullptr;
-	if (value == node->data.value) return node;
-	if (value < node->data.value) return search_impl(node->left, value);
-	return search_impl(node->right, value);
+	if (!root) {
+		root = new Node{
+			{value, {}},
+			nullptr,
+			nullptr
+		};
+	}
+	Node *curr = root, *prev = nullptr;
+	bool left = true; // just a boolean to know which direction to insert in
+	while (curr) {
+		prev = curr;
+		if (value <= curr->data.value) {
+			curr = curr->left;
+			left = true;
+		}
+		else if (value > curr->data.value) {
+			curr = curr->right;
+			left = false;
+		}
+	}
+	if (prev) {
+		Node *temp = new Node{
+			{value, {}},
+			nullptr,
+			nullptr
+		};
+		if (left) prev->left = temp;
+		else prev->right = temp;
+	}
 }
 
+// Searsh for a giving root in the tree and return its data struct pointer
 Root *Tree::search(const std::string& value) {
-	Node *res = search_impl(root, value);
-	if (res) return &(res->data);
-	else return nullptr;
+	Node *curr = root;
+	while (curr) {
+		if (value < curr->data.value) curr = curr->left;
+		else if (value > curr->data.value) curr = curr->right;
+		else return &(curr->data);
+	}
+	return nullptr;
 }
+
 // Display the tree (each root with its derivatives)
 void Tree::print() {
     std::cout << "\n====== ROOT TREE ======\n\n";
